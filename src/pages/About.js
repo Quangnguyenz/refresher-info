3a0eb902cbc012e940b30e21974a3e754f1241ea
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2021-11-25 17:38:03 -0600

    Initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>About Us</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat minus ab, corporis accusamus magnam quam, incidunt quisquam nostrum dolorum vero pariatur earum provident, porro minima veniam dolores odit ea atque.
+      </p>
+    </section>
+  )
+}
+
+export default About
